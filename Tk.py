dc91caa0d341137d56d95bfd618189c78aa91389
addasda49 <detalaukro@gmail.com>
2019-11-09 22:40:28 +0200

    Added main func, packing

--- Tk.py
+++ Tk.py
@@ -5,3 +5,17 @@ root = Tk()
 e = Entry(root, width=20)
 b = Button(root, text="Just a Button")
 l = Label(root, bg='black', fg='white', width=20)
+
+def str_to_list(event):
+    s = e.get()
+    s = s.split()
+    s.sort()
+    l['text'] = ' '.join(s)
+
+b.bind('<Button-1>', str_to_list)
+
+e.pack()
+b.pack()
+l.pack()
+
+root.mainloop()
